732afa832f88b880305e70ad04f5d76795c19f21
kevin.jantzer <kevin.jantzer@blackstoneaudio.com>
2015-02-25 11:05:30 -0800

    Adding grunt and npm to Model Editor

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+	require('jit-grunt')(grunt);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		
+		less: {
+			demo: {
+				options: {
+					compress: true,
+					yuicompress: true,
+					optimization: 2
+				},
+				files: {
+					"style.css": "style.less"
+				}
+			}
+		},
+		uglify: {
+
+			// non-minified version
+			development: {
+				src: [
+					'src/ModelEditor.js',
+					'src/Base.js',
+					'src/Input.js',
+					'src/RTE.js',
+					'src/Checkbox.js',
+					'src/Select.js'
+				],
+				dest: 'model-editor.js',
+				options: {
+					beautify: true
+				}
+			},
+			production: {
+				src: [
+					'src/ModelEditor.js',
+					'src/Base.js',
+					'src/Input.js',
+					'src/RTE.js',
+					'src/Checkbox.js',
+					'src/Select.js'
+				],
+				dest: 'model-editor.min.js',
+				options: {
+					banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy, HH:MM:ss") %> */\n',
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('default', ['less', 'uglify']);
+};
